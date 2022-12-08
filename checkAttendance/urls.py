52e1ff5fe609be0002bae699ecf2c89d99082a17
Sourabh044 <srb.sourabh.044@gmail.com>
2022-12-08 16:00:00 +0530

    Added Monthly Attendance search & Home page

--- checkAttendance/urls.py
+++ checkAttendance/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path , include
 from .views import *
 urlpatterns = [
-    path('', Home, name='Home'),
+    path('Home/', Home, name='Home'),
+    path('', base, name='base'),
     path('attendance/', AttendanceViewfunc, name='Attendance'),
     path('users/', Users, name='Users'),
-    path('attendance/list-view', AttendanceView.as_view(), name='Attendance-list-view'),
-    path('attendance/month-view', AttendanceMonthArchiveView.as_view(), name='Attendance-month-view'),
-        # Example: /2012/08/
-    path('attendance/month-view/<int:year>/<int:month>/',
-         AttendanceMonthArchiveView.as_view(month_format='%m'),
-         name="archive_month_numeric"),
-    # Example: /2012/aug/
-    path('<int:year>/<str:month>/',
-         AttendanceMonthArchiveView.as_view(),
-         name="archive_month"),
+    path('user-attendance/<int:pk>/',
+         UserMonthlyAttendance, name='User-Attendance'),
 ]
--- checkAttendance/views.py
+++ checkAttendance/views.py
@@ -1,77 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import calendar
 from datetime import date
-from django_tables2 import SingleTableView
-from .tables import AttendanceTable
-from .models import Attendance
-from django.views.generic.dates import MonthArchiveView
 from django.contrib.auth.models import User
-from .models import UserProfile
-from django.db.models.query import Prefetch
 from .forms import MonthlyAttendance
 
 # Create your views here.
 
 
+def base(request):
+    return redirect('Home')
+
 def Home(request):
     return render(request, 'base.html')
-
-
-def attendance(request):
-    return render(request, 'attendance.html')
-
-
-class AttendanceView(SingleTableView):
-    model = Attendance
-    table_class = AttendanceTable
-    template_name = 'attendance.html'
-
-
-class AttendanceMonthArchiveView(MonthArchiveView):
-    queryset = Attendance.objects.all()
-
-    def get_queryset(self):
-        return Attendance.objects.filter()
-    date_field = "attendance"
-    allow_future = True
-
-
-def AttendanceViewfunc(request):
-    month = request.GET.get('month', date.today().month)
-    year = date.today().year
-    form = MonthlyAttendance()
-    users = User.objects.all()
-    num_days = calendar.monthrange(date.today().year, int(month))[1]
-    print(num_days)
-    days = [date(year, int(month), day) for day in range(1, num_days+1)]
-    context = {
-        'users': users,
-        'days_list': days,
-        'month': calendar.month_name[int(month)],
-        'form': form,
-    }
-
-    # We have users all month attendance list in the i.attndance
-    # at = dict([(user,user.attendance.filter(attendance__month=month).all()) for user in users])
-    # print(at)
-    # nd = dict()
-    # for day in days:
-    #     for query in at.values():
-    #         for q in query:
-    #             if q.attendance.date() == day:
-    #                 nd[q.user] = q.attendance.date()
-    #                 # print(q.user,'->',q.attendance.date())
-    # print(nd)
-    # context['at'] = at
-    # @register.filter(name='gma')
-    # def get_monthly_attendance(user):
-    #     print('herer')
-    #     return user.attendance.filter(attendance__month=month).all()
-
-    # @register.filter
-    # def get_attendance(date,user):
-    #     return Attendance.objects.filter(user=user,attendance__date=date)
-    return render(request, 'attendancefunc.html', context)
 
 
 def Users(request):
@@ -82,3 +22,30 @@ def Users(request):
     }
 
     return render(request, 'users.html', context)
+
+
+def AttendanceViewfunc(request):
+    month = request.GET.get('month', date.today().month)
+    year = date.today().year
+    form = MonthlyAttendance()
+    users = User.objects.all()
+    num_days = calendar.monthrange(date.today().year, int(month))[1]
+    days = [date(year, int(month), day) for day in range(1, num_days+1)]
+    context = {
+        'users': users,
+        'days_list': days,
+        'month': calendar.month_name[int(month)],
+        'form': form,
+    }
+    return render(request, 'attendancefunc.html', context)
+
+
+def UserMonthlyAttendance(request, pk=None):
+    context = {}
+    if not pk:
+        return redirect('Attendance')
+    month = request.GET.get('month', date.today().month)
+    year = date.today().year
+    form = MonthlyAttendance()
+    user = User.objects.get(pk=pk)
+    return render('UserAttendance.html', context)
