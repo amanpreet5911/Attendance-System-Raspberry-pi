3953c94f7f15d4129d02eaa1698570e18430ea56
Sourabh044 <srb.sourabh.044@gmail.com>
2022-12-17 11:17:57 +0530

    Added late entry function

--- checkAttendance/templatetags/attendance_extras.py
+++ checkAttendance/templatetags/attendance_extras.py
@@ -1,22 +1,38 @@
 from django import template
 from accounts.models import Pi_attendance
-#from checkAttendance.models import Attendance
-from datetime import date
+from datetime import date, datetime
+import time
 register = template.Library()
 
 year = date.today().year
 month = date.today().month
+date_str = '10:00:00 AM'
+late_time = datetime.strptime(date_str, '%H:%M:%S %p')
 
 
 @register.filter(name='gla')
-def get_late_attendance(user, date):
-    # a = Pi_attendance.objects.filter(employee=user, clock_in__date=date, clock_in__time__gte=)
-
-    return a
+def get_late_attendance(user, date=None):
+    if date:
+        a = Pi_attendance.objects.filter(
+            employee=user, clock_in__date=date, clock_in__time__gte=late_time).first()
+        if a:
+            return a.clock_in.strftime('%H:%M:%S %p')
+        else:
+            return ''
+    else:
+        date = datetime.now().date()
+        a = Pi_attendance.objects.filter(
+            employee=user, clock_in__date=date, clock_in__time__gte=late_time).first()
+        if a:
+            return a.clock_in.strftime('%H:%M:%S %p')
+        else:
+            return ''
+    # return a.clock_in.strftime('%H:%M:%S %p')
 
 
 @register.filter(name='gmd')
 def get_attendance(user, date):
+    get_late_attendance(user)
     a = Pi_attendance.objects.filter(employee=user, clock_in__date=date)
     # print(a)
     # a =  Attendance.objects.filter(user=user,attendance__date=date).values_list('attendance',flat=True)
@@ -27,10 +43,10 @@ def get_attendance(user, date):
         if a.count() > 1:
             l = ''
             for i in a:
-                l += (i.clock_in.strftime('%m/%d/%Y, %H:%M:%S %p')) + '\n'
+                l += (i.clock_in.strftime('%H:%M:%S %p')) + '\n'
             return l
         else:
             l = list(a)[0]
-            return l.clock_in.strftime('%m/%d/%Y, %H:%M:%S %p')
+            return l.clock_in.strftime('%H:%M:%S %p')
 
     return 'No Data Available'
